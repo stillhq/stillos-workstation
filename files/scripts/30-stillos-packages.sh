de98ecdf29a1ec5b87b5523d4395b7470ace4449
PizzaLovingNerd <cameron@stillhq.io>
2025-06-08 17:05:26 -0700

    Update 30-stillos-packages.sh

--- files/scripts/30-stillos-packages.sh
+++ files/scripts/30-stillos-packages.sh
@@ -14,8 +14,8 @@ echo "Swapping GNOME"
 dnf swap -y gnome-shell https://download.copr.fedorainfracloud.org/results/still/stillos-alma/epel-10-x86_64/09130546-gnome-shell/gnome-shell-47.4-2.el10.alma.2.x86_64.rpm
 
 echo "Installing stillOS Packages"
-dnf install -y plymouth-theme-still-spinner still-control stillcenter swai swai-inst stillcount-client adw-gtk3-theme gnome-shell-extension-desktop-icons-ng
 dnf install -y https://kojipkgs.fedoraproject.org//packages/gnome-shell-extension-just-perfection/34.0/1.el10_1/noarch/gnome-shell-extension-just-perfection-34.0-1.el10_1.noarch.rpm
+dnf install -y plymouth-theme-still-spinner still-control stillcenter swai swai-inst stillcount-client adw-gtk3-theme gnome-shell-extension-desktop-icons-ng
 
 echo "Installing misc packages..."
 dnf install -y git lorax \

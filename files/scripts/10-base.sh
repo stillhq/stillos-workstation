4097604ce0a8f432ff39ee44dc8489e13872355b
PizzaLovingNerd <cameron@stillhq.io>
2025-06-13 12:05:29 -0700

    Go Time!

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -4,10 +4,11 @@ set -xeuo pipefail
 
 # Example of how you start customizing the image
 
+sudo dnf upgrade -y
+
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
 dnf config-manager --save --setopt=exclude=PackageKit,PackageKit-command-not-found,rootfiles,firefox
 
 dnf config-manager --add-repo https://gitlab.com/stillhq/stillOS/packages/stillos-release-final/-/raw/a10/stillos-alma.repo
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
-
